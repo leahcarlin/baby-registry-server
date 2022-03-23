19f34318304a511fb2ae3e869ee1833d2e3efc26
leahcarlin <leahrcarlin@gmail.com>
2022-03-23 18:05:59 +0100

    initial commit

---
+++ migrations/20220323170254-create-item.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      imgUrl: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      itemUrl: {
+        type: Sequelize.STRING
+      },
+      fulfilled: {
+        type: Sequelize.BOOLEAN
+      },
+      details: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('items');
+  }
+};
